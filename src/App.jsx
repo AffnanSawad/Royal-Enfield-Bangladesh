d3c58d5db95b716384e0c71b900b8f3d7ad038b6
Affnan Sawad <affnansawad2002@gmail.com>
2025-01-26 13:25:41 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div >
+        
+    <p> HELLO </p>
+
+
+    
+
+      </div>
+       
+    </>
+  )
+}
+
+export default App
